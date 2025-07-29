40cca18ae4396927ab9438979427ecc5ba3c2a08
Stawowcz <jstawowcz@gmail.com>
2025-07-29 21:59:23 +0800

    added structure of framework

---
+++ playwright.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig, devices } from "@playwright/test";
+import { env } from "@utils";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export default defineConfig({
+  testDir: "./src/tests/",
+  timeout: 30 * 1_000,
+  expect: {
+    timeout: 20 * 1_000,
+  },
+
+  reporter: [
+    ["list"],
+    ["junit", { outputFile: "test-results/junit-results.xml" }],
+    ["html", { outputFolder: "playwright-report", open: "never" }],
+  ],
+
+  use: {
+    headless: true,
+    baseURL: env.AUTOMATION_USER,
+    testIdAttribute: "data-qa",
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
+    trace: "on-first-retry",
+  },
+
+  projects: [
+    { name: "Chromium", use: { ...devices["Desktop Chrome"] } },
+    // { name: "Firefox",  use: { ...devices["Desktop Firefox"] } },
+    // { name: "WebKit",   use: { ...devices["Desktop Safari"] } },
+  ],
+
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 1 : 0,
+  workers: process.env.CI ? "100%" : "50%",
+
+  outputDir: "test-results/",
+});
---
+++ src/types/config/env-types.ts
@@ -0,0 +1,4 @@
+export type EnvVars = {
+  AUTOMATION_USER: string;
+
+};
