f1c61116aa5279f136258e27bf7550b412d7c035
Stawowcz <jstawowcz@gmail.com>
2025-08-12 23:00:33 +0800

    increased timeouts

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   testDir: "./src/tests/",
   timeout: 120 * 1_000,
   expect: {
-    timeout: 30 * 1_000,
+    timeout: 50 * 1_000,
   },
 
   reporter: [
@@ -60,7 +60,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
-  workers: process.env.CI ? "100%" : "50%",
+  workers: process.env.CI ? "100%" : "100%",
   globalSetup: "./global-setup",
 
   outputDir: "test-results/",
